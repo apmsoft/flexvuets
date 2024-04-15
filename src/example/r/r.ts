55cc9664fdf4ea2a99066f8edda88002c7eeebf5
fancyupsoft <apmsoft@gmail.com>
2024-04-15 22:35:54 +0900

    r.ts 예제 업데이트

--- src/example/r/r.ts
+++ src/example/r/r.ts
@@ -6,16 +6,17 @@ const onReady = () : void =>
     new App();
 
     // R 클래스를 import하고, sysmsg 리소스에 접근하여 사용할 수 있습니다.
-    Log.d(R.sysmsg); // sysmsg 리소스 데이터 출력
-    Log.d(R.arrays); // arrays 리소스 데이터 출력
-    Log.d(R.strings); // strings 리소스 데이터 출력
-    Log.d(R.numbers); // numbers 리소스 데이터 출력
+    Log.d(window.R.sysmsg); // sysmsg 리소스 데이터 출력
+    Log.d(window.R.arrays); // arrays 리소스 데이터 출력
+    Log.d(window.R.strings); // strings 리소스 데이터 출력
+    Log.d(window.R.numbers); // numbers 리소스 데이터 출력
 
 };
 
 document.addEventListener("DOMContentLoaded", () => {
     // R 클래스 초기화 후에 DOMContentLoaded 이벤트 발생
-    R.__init({
+    window.R = R;
+    window.R.__init({
         sysmsg : new URL(`../js/values/sysmsg${App.getLocale()}.js`, import.meta.url).href,
         arrays : new URL(`../js/values/arrays${App.getLocale()}.js`, import.meta.url).href,
         strings: new URL(`../js/values/strings${App.getLocale()}.js`, import.meta.url).href,
@@ -23,6 +24,6 @@ document.addEventListener("DOMContentLoaded", () => {
     }).then(() => {
         onReady();
     }).catch(err => {
-        console.error("Error initializing R:", err);
+        Log.e("Error initializing R:", err);
     });
 });
